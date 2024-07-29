500caf1791b1e4e1e6bba084f359d221d5922920
Tifany <tifsnunes@gmail.com>
2024-07-29 19:10:08 -0300

    finish analytical clock

--- script.js
+++ script.js
@@ -12,6 +12,14 @@ function updateClock() {
   digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(
     second
   )}`;
+
+  let sDeg = (360 / 60) * second - 90;
+  let mDeg = (360 / 60) * minute - 90;
+  let hDeg = (360 / 12) * hour - 90;
+
+  sElement.style.transform = `rotate(${sDeg}deg)`;
+  mElement.style.transform = `rotate(${mDeg}deg)`;
+  hElement.style.transform = `rotate(${hDeg}deg)`;
 }
 
 function fixZero(time) {
@@ -19,3 +27,4 @@ function fixZero(time) {
 }
 
 setInterval(updateClock, 1000);
+updateClock();
